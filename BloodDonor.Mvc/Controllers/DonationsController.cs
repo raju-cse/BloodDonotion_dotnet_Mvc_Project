4e71410bf1a796c1748bdab486c12dac0e1478d5
Your Name <your.email@example.com>
2025-07-26 23:09:10 +0600

    Add new DonatonsController

--- BloodDonor.Mvc/Controllers/DonationsController.cs
+++ BloodDonor.Mvc/Controllers/DonationsController.cs
@@ -26,7 +26,7 @@ namespace BloodDonor.Mvc.Controllers
         }
 
         // GET: Donations/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult>  Details(int? id)
         {
             if (id == null)
             {
